5ae700eeb5e03416de5857a8d09a05ccf03d7ac5
vencoDebeliq <vencarskibggaming@gmail.com>
2020-04-10 22:39:58 +0300

    Initial commit

---
+++ calc.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    string line[77];
+    ifstream myFile("vps.txt");
+    for (int i=0;getline(myFile,line[i]);++i)
+    {
+
+    }
+    int numberOfBoards;
+    int dif;
+    cout<<"Number of boards:";
+    cin>>numberOfBoards;
+    cout<<"\n IMP difference:";
+    cin>>dif;
+    if (dif>73)
+    {
+        cout<<"20.00\n";
+        return 0;
+    }
+    int a=(numberOfBoards-5)*5;
+    for (int i=a;i<a+5;++i)
+    {
+        cout<<line[dif-1][i];
+    }
+    cout<<'\n';
+
+    return 0;
+}
