18dd46169ff8f402528c433212f95dcdc0fb3e0c
Ventsislav Peychev <vencarskibggaming@gmail.com>
2020-09-12 04:28:41 +0300

    minors and going down

--- scoreCalc.cpp
+++ scoreCalc.cpp
@@ -2,27 +2,139 @@
 #include <string>
 #include <fstream>
 
+std::ifstream file;
+
+void outputTheFile()
+{
+    std::string l;
+    while (std::getline(file, l)) std::cout<<l<<'\n';
+}
+
 int main()
 {
     std::string score;
     std::getline(std::cin, score);
-    if (score=="x" || score=="X" || score=="double")
+    if (score=="x" or score=="X" or score=="double")
     {
-        std::string l;
-        std::ifstream file;
         file.open("double_scores.txt");
-        while (std::getline(file, l)) std::cout<<l<<'\n';
+        outputTheFile();
         return 0;
     }
     if (score=="pass")
     {
-        std::string l;
-        std::ifstream file;
         file.open("pass");
-        while (std::getline(file, l)) std::cout<<l<<'\n';
+        outputTheFile();
         return 0;
     }
-    
+    if (score=="xx" or score=="XX")
+    {
+        file.open("redouble");
+        outputTheFile();
+        return 0;
+    }
+    if (score[2]=='d') score[2]='c';
+    if (score[2]=='s') score[2]='h';
+    switch (score[0])
+    {
+        case '1':
+            switch (score[2])
+            {
+                case 'c':
+                    file.open("1club");
+                    outputTheFile();
+                    return 0;
+
+                case 'h':
+                    return 0;
+
+                case 'N':
+                    return 0;
+            }
+        case '2':
+            switch (score[2])
+            {
+                case 'c':
+                    file.open("2club");
+                    outputTheFile();
+                    return 0;
+
+                case 'h':
+                    return 0;
+
+                case 'N':
+                    return 0;
+            }
+        case '3':
+            switch (score[2])
+            {
+                case 'c':
+                    file.open("3club");
+                    outputTheFile();
+                    return 0;
+
+                case 'h':
+                    return 0;
+
+                case 'N':
+                    return 0;
+        }
+        case '4':
+            switch (score[2])
+            {
+                case 'c':
+                    file.open("4club");
+                    outputTheFile();
+                    return 0;
+
+                case 'h':
+                    return 0;
+
+                case 'N':
+                    return 0;
+            }
+        case '5':
+            switch (score[2])
+            {
+                case 'c':
+                    file.open("5club");
+                    outputTheFile();
+                    return 0;
+
+                case 'h':
+                    return 0;
+
+                case 'N':
+                    return 0;
+            }
+        case '6':
+            switch (score[2])
+            {
+                case 'c':
+                    file.open("6club");
+                    outputTheFile();
+                    return 0;
+
+                case 'h':
+                    return 0;
+                
+                case 'N':
+                    return 0;
+            }
+        case '7':
+            switch (score[2])
+            {
+                case 'c':
+                    file.open("7club");
+                    outputTheFile();
+                    return 0;
+
+                case 'h':
+                    return 0;
+                
+                case 'N':
+                    return 0;
+            }
+    }
 
 
     return 0;
